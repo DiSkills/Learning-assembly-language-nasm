5974d17827db8bce6adfa1d3ca1d87d4a86af753
DiSkills <anti.python.fastapi@gmail.com>
2023-03-24 13:36:52 +0300

    Added parenthesis balance

---
+++ test_parenthesis_balance.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+nasm -f elf parenthesis_balance.asm
+ld -m elf_i386 parenthesis_balance.o -o parenthesis_balance
+
+while read input output; do
+    result=`echo $input | ./parenthesis_balance`
+
+    if [ x"$output" != x"$result" ]; then
+        echo "TEST '$input' FAILED: expected '$output', got '$result'"
+    fi
+done <<END
+    5689874(123)()()()((()))l; YES
+    (((((((())))))))()(()) YES
+    )()()()( NO
+    )(()))) NO
+    (((((())( NO
+    f YES
+    ( NO
+    ) NO
+END
+
+rm parenthesis_balance.o parenthesis_balance
