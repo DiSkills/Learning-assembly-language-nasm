8959dd0b7b61a106d8031a367130601b127bdb95
DiSkills <anti.python.fastapi@gmail.com>
2023-03-25 12:29:49 +0300

    Refactoring...

--- max_length.asm
+++ max_length.asm
@@ -2,26 +2,26 @@
 global _start
 
 section .text
-_start: xor ebx, ebx   ; clear current length
-        xor ecx, ecx   ; clear maximum length
-input:  GETCHAR        ; input char
-        cmp al, -1     ; if end of file
-        je finish      ; go to finish
-        cmp al, ' '    ; if space
-        je max         ; go to max
-        cmp al, 10     ; if \n
-        je max         ; go to max
-        inc ebx        ; if char not in (' ', '\n', eof) then ebx += 1
-        jmp input      ; go to next char
-max:    cmp ebx, ecx   ; if ebx <= ecx
-        jna say        ; go to next word
-        mov ecx, ebx   ; if ebx > ecx then ecx = ebx
-say:    xor ebx, ebx   ; clear current length
-line:   cmp al, 10     ; if not new line
-        jne input      ; go to next char
-        jecxz input    ; if ecx == 0 -> go to next char
-lp:     PRINT "*"      ; print stars
-        loop lp        ;
-        PUTCHAR 10     ; print('\n')
-        jmp input      ; go to next char
-finish: FINISH         ; finish
+_start:     xor ebx, ebx        ; clear current length
+            xor ecx, ecx        ; clear maximum length
+input:      GETCHAR             ; input char
+            cmp al, -1          ; if end of file
+            je finish           ; go to finish
+            cmp al, ' '         ; if space
+            je max              ; go to max
+            cmp al, 10          ; if \n
+            je max              ; go to max
+            inc ebx             ; if char not in (' ', '\n', eof) then ebx += 1
+            jmp input           ; go to next char
+max:        cmp ebx, ecx        ; if ebx <= ecx
+            jna say             ; go to next word
+            mov ecx, ebx        ; if ebx > ecx then ecx = ebx
+say:        xor ebx, ebx        ; clear current length
+line:       cmp al, 10          ; if not new line
+            jne input           ; go to next char
+            jecxz input         ; if ecx == 0 -> go to next char
+lp:         PRINT "*"           ; print stars
+            loop lp             ;
+            PUTCHAR 10          ; print('\n')
+            jmp input           ; go to next char
+finish:     FINISH              ; finish
--- parenthesis_balance.asm
+++ parenthesis_balance.asm
@@ -2,34 +2,34 @@
 global _start
 
 section .text
-_start:         xor ebx, ebx        ; clear the left parenthesis counter
-                xor ecx, ecx        ; clear the right parenthesis counter
-                xor edx, edx        ; balance is maintained
-char:           GETCHAR             ;
-                cmp al, -1          ; char == eof
-                je finish           ; go to finish
-                cmp al, 10          ; char == \n
-                je line             ; go to line
-                cmp al, '('         ; char != '('
-                jne right           ; go to right
-                inc ebx             ; ebx++
-                jmp char            ; go to next char
-right:          cmp al, ')'         ; char != ')'
-                jne char            ; go to next char
-                inc ecx             ; ecx++
-                cmp ebx, ecx        ; ebx >= ecx - '(' >= ')'
-                jnb char            ; go to next char
-                mov edx, 1          ; balance is not maintained
-                jmp char            ; go to next char
-line:           sub ebx, ecx        ; ebx == ecx -> ebx == 0
-                or edx, ebx         ; edx == ebx == 0 -> balance is maintained
-                jnz false           ;
-                PRINT "YES"         ;
-                jmp new_line        ; clear
-false:          PRINT "NO"          ;
-new_line:       xor ebx, ebx        ; clear the left parenthesis counter
-                xor ecx, ecx        ; clear the right parenthesis counter
-                xor edx, edx        ; balance is maintained
-                PUTCHAR 10          ; print('\n')
-                jmp char            ; go to next char
-finish:         FINISH
+_start:     xor ebx, ebx        ; clear the left parenthesis counter
+            xor ecx, ecx        ; clear the right parenthesis counter
+            xor edx, edx        ; balance is maintained
+char:       GETCHAR             ;
+            cmp al, -1          ; char == eof
+            je finish           ; go to finish
+            cmp al, 10          ; char == \n
+            je line             ; go to line
+            cmp al, '('         ; char != '('
+            jne right           ; go to right
+            inc ebx             ; ebx++
+            jmp char            ; go to next char
+right:      cmp al, ')'         ; char != ')'
+            jne char            ; go to next char
+            inc ecx             ; ecx++
+            cmp ebx, ecx        ; ebx >= ecx - '(' >= ')'
+            jnb char            ; go to next char
+            mov edx, 1          ; balance is not maintained
+            jmp char            ; go to next char
+line:       sub ebx, ecx        ; ebx == ecx -> ebx == 0
+            or edx, ebx         ; edx == ebx == 0 -> balance is maintained
+            jnz false           ;
+            PRINT "YES"         ;
+            jmp new_line        ; clear
+false:      PRINT "NO"          ;
+new_line:   xor ebx, ebx        ; clear the left parenthesis counter
+            xor ecx, ecx        ; clear the right parenthesis counter
+            xor edx, edx        ; balance is maintained
+            PUTCHAR 10          ; print('\n')
+            jmp char            ; go to next char
+finish:     FINISH              ;
