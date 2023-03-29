8f84d2b88efc0e7b4059106516c5cac8fb4aad74
DiSkills <anti.python.fastapi@gmail.com>
2023-03-29 20:24:39 +0300

    Changed input

--- test_last_word_length.sh
+++ test_last_word_length.sh
@@ -3,20 +3,20 @@
 nasm -f elf last_word_length.asm
 ld -m elf_i386 last_word_length.o -o last_word_length
 
-while read input output; do
-    result=`echo $input | ./last_word_length`
+while IFS="|" read input output; do
+    result=`echo "$input" | ./last_word_length`
 
     if [ x"$output" != x"$result" ]; then
         echo "TEST '$input' FAILED: expected '$output', got '$result'"
     fi
 done <<END
-    2356121\ 2\ 1\ 30 **
-    \ \ \ \ \ 5 *
-    56\ \ \ \ \ = *
-    .f\ \ \ \ \ \  **
-    \ \ \ \ \ \ \ 
+2356121\ 2\ 1\ 30|**
+\ \ \ \ \ 5|*
+56\ \ \ \ \ =|*
+.f\ \ \ \ \ \ |**
+\ \ \ \ \ \ \ 
 
-    \ \ \ cvf\ \ \  ***
+\ \ \ cvf\ \ \ |***
 END
 
 rm last_word_length.o last_word_length
--- test_word_count.sh
+++ test_word_count.sh
@@ -3,17 +3,17 @@
 nasm -f elf word_count.asm
 ld -m elf_i386 word_count.o -o word_count
 
-while read input output; do
-    result=`echo $input | ./word_count`
+while IFS="|" read input output; do
+    result=`echo "$input" | ./word_count`
 
     if [ x"$output" != x"$result" ]; then
         echo "TEST '$input' FAILED: expected '$output', got '$result'"
     fi
 done <<END
-    Hello,\ World! **
-    Hello.\ How\ are\ you\ doing? *****
-    I\ am\ learning\ assembly\ \ \ \ language\ \ \ \ \  *****
-    \ \ \ \ \ \ \ Tests\ \ \ \ \ are\ everything!\  ***
+Hello,\ World!|**
+Hello.\ How\ are\ you\ doing?|*****
+I\ am\ learning\ assembly\ \ \ \ language\ \ \ \ \ |*****
+\ \ \ \ \ \ \ Tests\ \ \ \ \ are\ everything!\ |***
 END
 
 rm word_count.o word_count
