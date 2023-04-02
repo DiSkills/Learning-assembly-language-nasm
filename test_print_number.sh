1f24cbb0e213e5070fd64c33173be6abff71ddaa
DiSkills <anti.python.fastapi@gmail.com>
2023-04-02 14:21:42 +0300

    Added empty test

--- test_print_number.sh
+++ test_print_number.sh
@@ -4,6 +4,10 @@ nasm -f elf print_number.asm
 ld -m elf_i386 print_number.o -o print_number
 
 while read input output; do
+    if [ x"$input" == x"\\0" ]; then
+        input=""
+    fi
+
     result=`echo -n "$input" | ./print_number`
 
     if [ x"$output" != x"$result" ]; then
@@ -22,6 +26,7 @@ done <<END
     ********** 10
     *********** 11
     **\ ************************************************************************************************************* 112
+    \\\0 0
 END
 
 rm print_number.o print_number
