5fffb4c383669906e3b1eb79baff6611484447bf
DiSkills <anti.python.fastapi@gmail.com>
2023-04-02 13:24:39 +0300

    Added print number

---
+++ test_print_number.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+nasm -f elf print_number.asm
+ld -m elf_i386 print_number.o -o print_number
+
+while read input output; do
+    result=`echo -n "$input" | ./print_number`
+
+    if [ x"$output" != x"$result" ]; then
+        echo "TEST '$input' FAILED: expected '$output', got '$result'"
+    fi
+done <<END
+    * 1
+    ** 2
+    *** 3
+    **** 4
+    ***** 5
+    ****** 6
+    ******* 7
+    ******** 8
+    ********* 9
+    ********** 10
+    *********** 11
+    **\ ************************************************************************************************************* 112
+END
+
+rm print_number.o print_number
