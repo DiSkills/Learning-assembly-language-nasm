b61aed0169ce23ca88ea8dda607458daa918e055
DiSkills <anti.python.fastapi@gmail.com>
2023-04-12 16:08:31 +0300

    Added sum difference product of 2 numbers

--- sum_difference_product_of_2_numbers.asm
+++ sum_difference_product_of_2_numbers.asm
@@ -3,13 +3,19 @@ global _start
 
 section .data
 space db ' '
-number dd 0
-quantity dd 2
-end_line db 10
 undefined dd -1
+end_line db 10
+
+number dd 0
+number2 dd 0
+
+result_count dd 3
+quantity dd 2
 number_system dd 10
 
 section .bss
+result resd 3
+digits resb 10
 numbers resd 2
 separators resb 2
 
@@ -50,10 +56,50 @@ not_a_number:
             mov eax, [number]
             mov [edi + ecx * 4 - 4], eax
             loop input
-success:
-            PRINT "Success"
+get_indexes:
+            mov esi, numbers
+            mov edi, result
+get_numbers:
+            mov eax, [esi + 4]
+            mov [number], eax
+            mov eax, [esi]
+            mov [number2], eax
+sum:
+            add eax, [number]
+            mov [edi + 2 * 4], eax
+difference:
+            mov eax, [number]
+            sub eax, [number2]
+            mov [edi + 1 * 4], eax
+product:
+            mov eax, [number]
+            mul dword [number2]
+            mov [edi], eax
+output:
+            mov esi, result
+            mov edi, digits
+            mov ecx, [result_count]
+print_number:
+            xor ebx, ebx
+            mov eax, [esi + ecx * 4 - 4]
+digit:
+            xor edx, edx
+            div dword [number_system]
+            add edx, '0'
+            mov [edi + ebx * 1], edx
+            inc ebx
+            cmp eax, 0
+            jne digit
+print:
+            PUTCHAR [edi + ebx * 1 - 1]
+            dec ebx
+            cmp ebx, 0
+            jne print
+next:
+            PUTCHAR [space]
+            loop print_number
             jmp finish
 error:
             PRINT "Error"
-finish:     PUTCHAR 10
+finish:     PUTCHAR [end_line]
             FINISH
