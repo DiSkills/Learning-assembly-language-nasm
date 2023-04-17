b961e62d181afe431dd2594b06015dcf43241e72
DiSkills <anti.python.fastapi@gmail.com>
2023-04-17 16:28:42 +0300

    Refactored output of numbers

--- sum_difference_product_of_2_numbers.asm
+++ sum_difference_product_of_2_numbers.asm
@@ -39,11 +39,11 @@ separators_initialization:
         mov ecx, [input_size]        ; number of separators
         mov al, [space]              ; separator by default is space
 add_separator:
-        mov [separators + ecx * 1 - 1], al ; adding the current separator
+        mov [separators + ecx * 1 - 1], al ; add the current separator
         loop add_separator
 changing_last_separator:
         mov al, [end_line]           ; last separator by default is <CR>
-        mov [separators], al         ; changing the last separator
+        mov [separators], al         ; change the last separator
 
 getting_input_data:
         mov ecx, [input_size]        ; number of input data
@@ -53,7 +53,7 @@ number_entry:
         mov [number], dword 0        ; number by default is 0
         mov ebx, [undefined]         ; current digit by default is undefined
 character_processing:
-        GETCHAR                      ; character reading
+        GETCHAR                      ; character read
         sub al, [zero]               ; subtract zero code from character
         cmp eax, 9                   ; if the character is not a digit
         jnbe processing_not_a_number ; then we treat it as not a number
@@ -65,10 +65,10 @@ character_processing:
         jmp character_processing     ; process the next character
 processing_not_a_number:
         cmp ebx, [undefined]         ; if there were no digits
-        je error                     ; then throw an error
+        je error_output              ; then throw an error
         add al, [zero]               ; add zero code to get character
         cmp [separators + ecx * 1 - 1], al ; if the character is not a separator
-        jne error                    ; then throw an error
+        jne error_output             ; then throw an error
         mov eax, [number]            ; current number
         mov [input_data + ecx * 4 - 4], eax ; save the current number
         dec ecx                      ; one number entered
@@ -90,31 +90,32 @@ getting_product:
         mov eax, [number]            ; get the first number
         mul dword [number2]          ; number1 * number2
         mov [output_data], eax       ; save the product of numbers
+
 output:
-        mov esi, output_data
-        mov edi, digits
-        mov ecx, [output_size]
-print_number:
-        xor ebx, ebx
-        mov eax, [esi + ecx * 4 - 4]
-digit:
-        xor edx, edx
-        div dword [number_system]
-        add edx, '0'
-        mov [edi + ebx * 1], dl
-        inc ebx
-        cmp eax, 0
-        jne digit
-print:
-        PUTCHAR [edi + ebx * 1 - 1]
-        dec ebx
-        cmp ebx, 0
-        jne print
-next:
-        PUTCHAR [space]
-        loop print_number
-        jmp finish
-error:
+        mov ecx, [output_size]       ; number of output data
+number_output:
+        xor ebx, ebx                 ; clear the number of digits
+        mov eax, [output_data + ecx * 4 - 4] ; get the current number
+digit_processing:
+        xor edx, edx                 ; clear the current discharge
+        div dword [number_system]    ; get the last digit of a number
+        add dl, [zero]               ; add zero code to get character
+        mov [digits + ebx * 1], dl   ; save the current digit
+        inc ebx                      ; increase the number of digits
+        cmp eax, 0                   ; if there are still digits
+        jne digit_processing         ; then process the next digit
+print_digit:
+        PUTCHAR [digits + ebx * 1 - 1] ; output the current digit
+        dec ebx                      ; another digit of the number printed
+        cmp ebx, 0                   ; if not all digits are printed yet
+        jne print_digit              ; then output the following
+next_number:
+        dec ecx                      ; one number out
+        jecxz finish                 ; if the numbers are out, then finish the job
+        PUTCHAR [space]              ; output space (separator)
+        jmp number_output            ; output the next number
+error_output:
         PRINT "Error"
-finish:     PUTCHAR [end_line]
+finish:
+        PUTCHAR [end_line]           ; line break output
         FINISH
