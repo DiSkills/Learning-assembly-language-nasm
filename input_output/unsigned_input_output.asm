3c4730e1a233eac4f5a5f26511ba0eaba55a8eaa
DiSkills <anti.python.fastapi@gmail.com>
2024-03-01 19:25:20 +0300

    Added input/output numbers

--- input_output/unsigned_input_output.asm
+++ input_output/unsigned_input_output.asm
@@ -2,33 +2,21 @@
 global _start                   ; делаем точку входа глобальной
 
 extern input_number             ; подключаем ввод
-extern convert_number_to_string ; подключаем вывод
-extern print_string
+extern output_number            ; подключаем вывод
 
 
 section .bss
-string resb 11
 number resd 1
-
-
-section .data
-max_length dd 11
 
 
 section .text
 _start:
+; input
         push dword number
         call input_number
-        
-        nop
 ; output
         push dword [number]
-        push dword string
-        call convert_number_to_string
-
-        push dword string
-        push ecx
-        call print_string
-        PUTCHAR 10
+        call output_number
 finish:
+        PUTCHAR 10
         FINISH
